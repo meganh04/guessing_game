58533f7723b84ca02e0cc916e28f50fbc72833a7
Megan Healey <healeymt@vcu.edu>
2025-10-07 20:47:03 -0400

    Implement On Speak Event

--- script.js
+++ script.js
@@ -18,9 +18,19 @@ recognition.start();
 
 // Capture user speak
 function onSpeak(event) {
+    console.log(event);
   const msg = event.results[0][0].transcript;  // You can log the event to view the structure of the data
+  
   console.log(msg);
 }
 
 // Speak result
 recognition.addEventListener('result', onSpeak);
+
+// Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
